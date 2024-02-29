0e24da89c4787c3007e4227706cae424de639d76
Richard Hendricks <richardhendricks611@gmail.com>
2024-02-29 18:56:20 +0100

    fix: change SOLIDX token image

--- src/layout/header/components/PriceView.tsx
+++ src/layout/header/components/PriceView.tsx
@@ -1,19 +1,20 @@
 import { Stack, Typography } from "@mui/material";
 
+import Logo from "../../../assets/icons/LogoWithoutText";
+
 function PriceView() {
   return (
     <Stack direction="row" spacing={1} alignItems="center">
       <Stack
         width={38}
         height={38}
-        bgcolor="#252525"
+        bgcolor="#000000"
         borderRadius="100%"
+        border={"1px solid #333333"}
         alignItems="center"
         justifyContent="center"
       >
-        <Typography fontSize="0.75rem" fontWeight={700}>
-          SD
-        </Typography>
+        <Logo />
       </Stack>
       <Typography fontSize="0.75rem" fontWeight={600}>
         $1.056
